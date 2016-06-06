fc4bacb0e022043ed3090b682381a3d8959239ed
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-06-06 09:07:20 +0200

    añadimos el primer metodo con refactor, para tener un metodo donde se nos muestre el valor de n

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -9,12 +9,17 @@ public class Prin {
 public static void main(String[] args) throws IOException{
     
     
-Principal obj = new Principal(10,20);
-int n = obj.i;
-System.out.println("Es: " + n);
+        VisualizacionVariN();
 Principal nm = new Principal(10);
 int m = nm.i;
 System.out.println("Es: " + m);
 Principal mn = new Principal();
 int nn = mn.i;
-System.out.println("Es: " + nn);}}
+System.out.println("Es: " + nn);}
+
+    public static void VisualizacionVariN() {
+        Principal obj = new Principal(10,20);
+        int n = obj.i;
+        System.out.println("Es: " + n);
+    }
+}
