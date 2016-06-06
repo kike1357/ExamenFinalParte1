b07e3bdd56a851e09ea9f88ef5ac18bd2b3fbf3a
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-06-06 09:31:05 +0200

    Añadido el javadoc en la clase Principal1

--- src/examenfinalparte1/Principal1.java
+++ src/examenfinalparte1/Principal1.java
@@ -13,22 +13,30 @@ public static void main(String[] args) throws IOException{
         VisualizacionVariM();
         VisualizacionVarNN();
 }
-
+/**
+     * Metodo que se usa para visualizar el valor final de la variable nn con un System.out.println
+     */
     public static void VisualizacionVarNN() {
-        Principal mn = new Principal();
-        int nn = mn.i;
+        Principal2 mn = new Principal2();
+        int nn = mn.q;
         System.out.println("Es: " + nn);
     }
 
+ /**
+     * Metodo que se usa para visualizar el valor final de la variable m con un System.out.println
+     */   
     public static void VisualizacionVariM() {
-        Principal nm = new Principal(10);
-        int m = nm.i;
+        Principal2 nm = new Principal2(10);
+        int m = nm.q;
         System.out.println("Es: " + m);
     }
-
+    
+/**
+     * Metodo que se usa para visualizar el valor final de la variable n con un System.out.println
+     */
     public static void VisualizacionVariN() {
-        Principal obj = new Principal(10,20);
-        int n = obj.i;
+        Principal2 obj = new Principal2(10,20);
+        int n = obj.q;
         System.out.println("Es: " + n);
     }
 }
