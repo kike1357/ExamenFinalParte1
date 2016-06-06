fc9fca1985aa03be1db8a02b1511ee4156c0c1ac
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-06-06 09:09:01 +0200

    añadimos el segundo metodo con refactor, metodo donde sabemos el valor de la variable m

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -10,12 +10,16 @@ public static void main(String[] args) throws IOException{
     
     
         VisualizacionVariN();
-Principal nm = new Principal(10);
-int m = nm.i;
-System.out.println("Es: " + m);
+        VisualizacionVariM();
 Principal mn = new Principal();
 int nn = mn.i;
 System.out.println("Es: " + nn);}
+
+    public static void VisualizacionVariM() {
+        Principal nm = new Principal(10);
+        int m = nm.i;
+        System.out.println("Es: " + m);
+    }
 
     public static void VisualizacionVariN() {
         Principal obj = new Principal(10,20);
